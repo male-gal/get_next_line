0ca582ce1a9942d26d97adaa3bbc9bdcfb710c14
male-gal <male-gal@student.42.fr>
2018-04-25 16:54:51 +0200

    Push GNL (Final ?)

--- get_next_line.c
+++ get_next_line.c
@@ -6,37 +6,43 @@
 /*   By: male-gal <male-gal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/11 15:30:47 by male-gal          #+#    #+#             */
-/*   Updated: 2018/04/24 19:16:04 by male-gal         ###   ########.fr       */
+/*   Updated: 2018/04/25 16:52:03 by male-gal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char			*ft_strrejoin(char *s1, char *s2, size_t len)
+static char			*ft_str_joiner(char *s1, char *s2, size_t len)
 {
-	char		*out;
-	char		*mem;
 	int			i;
+	char		*out;
+	int			index;
 
+	index = 0;
 	i = ft_strlen(s1) + ++len;
 	out = ft_strnew(i);
-	mem = out;
 	while (*s1)
-		*out++ = *s1++;
+	{
+		out[index] = *s1++;
+		index++;
+	}
 	while (*s2 && --len > 0)
-		*out++ = *s2++;
-	*out = '\0';
-	return (out - (out - mem));
+	{
+		out[index] = *s2++;
+		index++;
+	}
+	out[index] = '\0';
+	return (out);
 }
 
 static char			*ft_strchrcpy(char *save)
 {
-	if (ft_strchr(save, '\n'))
+	if (ft_strchr(save, SEPARATOR))
 	{
-		ft_strcpy(save, ft_strchr(save, '\n') + 1);
+		ft_strcpy(save, ft_strchr(save, SEPARATOR) + 1);
 		return (save);
 	}
-	if (ft_strlen_untilc(save, '\n') > 0)
+	if (ft_strlen_untilc(save, SEPARATOR) > 0)
 	{
 		ft_strcpy(save, ft_strchr(save, '\0'));
 		return (save);
@@ -47,23 +53,22 @@ static char			*ft_strchrcpy(char *save)
 int					get_next_line(int const fd, char **line)
 {
 	char		buff[BUFF_SIZE + 1];
-	static char	*save[256];
+	static char	*save[OPEN_MAX];
 	int			res;
 	char		*ptr;
 
-	if (!line || fd <= -1 || BUFF_SIZE < 1 || read(fd, buff, 0) < 0)
+	if (!line || fd <= -1 || BUFF_SIZE < 1 || read(fd, buff, 0) < 0 ||
+		(!(save[fd]) && (save[fd] = ft_strnew(0)) == NULL))
 		return (-1);
-	if (!(save[fd]) && (save[fd] = ft_strnew(0)) == NULL)
-		return (-1);
-	while (!(ft_strchr(save[fd], '\n')) && (res =
+	while (!(ft_strchr(save[fd], SEPARATOR)) && (res =
 		read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[res] = '\0';
 		ptr = save[fd];
-		save[fd] = ft_strrejoin(ptr, buff, res);
+		save[fd] = ft_str_joiner(ptr, buff, res);
 		free(ptr);
 	}
-	*line = ft_strsub(save[fd], 0, ft_strlen_untilc(save[fd], '\n'));
+	*line = ft_strsub(save[fd], 0, ft_strlen_untilc(save[fd], SEPARATOR));
 	if (ft_strchrcpy(save[fd]) == NULL)
 		return (0);
 	return (1);
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,16 @@
 /*   By: male-gal <male-gal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/11 15:32:42 by male-gal          #+#    #+#             */
-/*   Updated: 2018/04/24 19:17:04 by male-gal         ###   ########.fr       */
+/*   Updated: 2018/04/25 16:25:34 by male-gal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 9999
+# define SEPARATOR '\n'
 # include "libft/libft.h"
+# include <limits.h>
 
 int		get_next_line(int const fd, char **line);
 #endif
