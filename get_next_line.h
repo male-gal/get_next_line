59f6c7d4be9afaa30f992bdb877c7c589a6ac528
male-gal <male-gal@student.42.fr>
2018-04-25 17:03:44 +0200

    Changement de la BUFF_SIZE pour le push

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: male-gal <male-gal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/11 15:32:42 by male-gal          #+#    #+#             */
-/*   Updated: 2018/04/25 16:25:34 by male-gal         ###   ########.fr       */
+/*   Updated: 2018/04/25 16:57:20 by male-gal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 9999
+# define BUFF_SIZE 32
 # define SEPARATOR '\n'
 # include "libft/libft.h"
 # include <limits.h>
